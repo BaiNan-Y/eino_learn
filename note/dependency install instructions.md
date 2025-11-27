7e09f69108105270ff012792aa33948eefadfc71
Qing He <M_Bainan@outlook.com>
2025-11-27 17:23:48 +0800

    20251127 first test

---
+++ note/dependency install instructions.md
@@ -0,0 +1,14 @@
+### 环境信息读取
+
+```
+go get "github.com/joho/godotenv"
+```
+
+### eino相关
+
+```
+go get "github.com/cloudwego/eino/schema"
+go get "github.com/cloudwego/eino/components/model"
+go get "github.com/cloudwego/eino-ext/components/model/ark"
+```
+
---
+++ project/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino/schema"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	model, err := ark.NewChatModel(ctx, &ark.ChatModelConfig{
+		APIKey: os.Getenv("ARK_API_KEY"),
+		Model:  os.Getenv("MODEL"),
+	})
+	input := []*schema.Message {
+		schema.SystemMessage("你是一个可爱的高中女生"),
+		schema.UserMessage("你好"),
+	}
+	response, err := model.Generate(ctx, input)
+	if err != nil {
+		panic(err)
+	}
+	print(response.Content)
+}
